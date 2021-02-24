ea10901fc0d39ff858521976ee6c803bc7c70ef9
Crami <cramitus@gmail.com>
2021-02-24 14:34:33 +0100

    Input del player
    He creado el script de HellbotInput donde ya estan puestos todos los controles de las acciones que puede hacer el jugador

--- HELLBOTS/Assets/Scripts/HellbotInput.cs
+++ HELLBOTS/Assets/Scripts/HellbotInput.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class HellbotInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static float Vertical{
+        get { return Input.GetAxis("Vertical"); }
+    }
+    /* Para llamarlo simplemente es crear una variable e igualarlo al valor que devuelven las funciones
+     *  private float vertical;
+     *  vertical = HellbotInput.Vertical;
+     *  y cuando se quiera usar la variable se utilizara solo "veritcal"
+     */
+    public static float Horizontal
     {
-        
+        get { return Input.GetAxis("Horizontal"); }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public static bool Crouch
     {
-        
+        get { return Input.GetKey(KeyCode.S); }
     }
+    /*Aqui seria igual que en el de vertical solo que la variable ha de ser un booleano
+     *  private bool crouch
+     *  crouch = HellbotInput.Crouch
+     */
+    public static bool Interact
+    {
+        get { return Input.GetKey(KeyCode.E); }
+    }
+
+    public static bool Heal
+    {
+        get { return Input.GetKey(KeyCode.Q); }
+    }
+
+    public static bool Jump
+    {
+        get { return Input.GetKey(KeyCode.Space); }
+    }
+
+    public static bool Melee
+    {
+        get { return Input.GetMouseButton(1); }
+        //esto es el click izquierdo
+    }
+
+    public static bool Shoot
+    {
+        get { return Input.GetMouseButton(0); }
+        //esto es el click derecho
+    }
+
+    public static bool PowerUp
+    {
+        get { return Input.GetKey(KeyCode.F); }
+    }
+
 }
