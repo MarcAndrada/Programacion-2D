68c84077b9391495898889cc21d2b3081781892a
Crami <cramitus@gmail.com>
2021-02-28 13:05:36 +0100

    Apuntado del Arma
    He creado una script que hace invisible al cursor y indica donde esta con una mira (muy simple), tambien he ajustado un poco las posiciones z de los enemigos para que la mira quede por encima de ellos pero no encima del player

--- HELLBOTS/Assets/Scripts/HellbotController.cs
+++ HELLBOTS/Assets/Scripts/HellbotController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class HellbotController : MonoBehaviour
 {
+
+
+    public Transform Crosshair;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ocultar el cursor, cambiar de sitio mas adelante
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Crosshair.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
     }
 }
