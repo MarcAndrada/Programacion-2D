495bf41851e8795019e88b5e99b75114498da9aa
Joel <joel.cortes@enti.cat>
2021-03-08 20:28:16 +0100

    Sniper
    He puesto el snipa, dispara y apunta a donde esta el juegador(se puede cambiar la velocidad de la bala y el fireRate)

---
+++ HELLBOTS/Assets/Scripts/Sniper_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper_Controller : MonoBehaviour {
+
+    public GameObject bala;
+    public float fireRate;
+    public float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkIfTimeToFire();
+    }
+
+    void checkIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(bala, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+
+}
