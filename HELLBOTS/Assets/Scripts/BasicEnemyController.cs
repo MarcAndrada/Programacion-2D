f250274644f129ca192af72601e16c709ecdd9ce
Didac <didacsegarra@gmail.com>
2021-03-09 18:12:25 +0100

    Enemigo1 + Disparo
    Enemigo1 + Disparo

---
+++ HELLBOTS/Assets/Scripts/BasicEnemyController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour
+{
+    public enum Direction { NONE, LEFT, RIGHT }
+    public float speed;
+    public Direction basicEnemyDirection = Direction.NONE;
+    public GameObject bala;
+    public float fireRate;
+    public float nextFire;
+
+    private int firstDir;
+    private float currentSpeed;
+    private Rigidbody2D rigidB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireRate = 1f;
+        nextFire = Time.time;
+        firstDir = Random.Range(1, 2);
+        rigidB = GetComponent<Rigidbody2D>();
+        if (firstDir == 1)
+        {
+            basicEnemyDirection = Direction.LEFT;
+        }
+        else if (firstDir == 2)
+        {
+            basicEnemyDirection = Direction.RIGHT;
+        }
+    }
+
+    // Update is called once per frame
+
+
+    void FixedUpdate()
+    {
+        float delta = Time.fixedDeltaTime * 1000;
+        switch (basicEnemyDirection)
+        {
+            default:
+                break;
+            case Direction.LEFT:
+                currentSpeed = -speed;
+
+                break;
+            case Direction.RIGHT:
+                currentSpeed = speed;
+
+                break;
+
+        }
+
+        rigidB.velocity = new Vector2(currentSpeed, -rigidB.gravityScale * delta);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            if (basicEnemyDirection == Direction.RIGHT)
+            {
+                basicEnemyDirection = Direction.LEFT;
+            }
+            else if (basicEnemyDirection == Direction.LEFT)
+            {
+                basicEnemyDirection = Direction.RIGHT;
+            }
+        }
+
+        if (collision.gameObject.tag == "Hellbot")
+        {
+            collision.gameObject.SetActive(false);
+        }
+
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkIfTimeToFire();
+    }
+
+    void checkIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(bala, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
---
+++ HELLBOTS/Assets/Scripts/Fire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public Rigidbody2D Bala2;
+    public Transform Barrel;
+    private float Bala2Speed = 500000f;
+
+    private float fireRate = 1f;
+    private float nextFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            var spawnedBala2 = Instantiate(Bala2, Barrel.position, Barrel.rotation);
+            spawnedBala2.AddForce(Barrel.right * Bala2Speed);
+        }
+    }
+}
